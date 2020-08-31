df7c435485d119e39af60265837ea52adff58479
YiJaeE <jinnn2181@gmail.com>
2020-09-01 02:43:58 +0900

    feat: add global styles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *, *::before, *::after {
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration:none;
+  color:inherit;
+  cursor: pointer;
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  font-size: 14rem;
+  background-color: #262626;
+  font-family: -apple-system, system-ui,BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,sans-serif;
+}
+
+ol, ul, li {
+  list-style: none;
+}
+
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+input, button {
+  background-color: transparent;
+}
+
+button {
+  cursor: pointer;
+}
+`;
+
+export default GlobalStyles;
