d8925da29d0ea89747570959fccdde0dad1ef3e4
YiJaeE <jinnn2181@gmail.com>
2020-09-02 01:26:26 +0900

    test: svg test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Test from './components/Test';
+import SvgTest from './styles/SvgTest';
 
 function App() {
   return (
     <div>
       <h1>I'mstagram</h1>
       <Test />
+      <SvgTest />
     </div>
   );
 }
