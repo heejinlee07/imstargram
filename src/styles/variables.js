d789315d0f7a0c74b1f2610a8dddc5718a316b36
heejin lee <heejinlee07@gmail.com>
2020-09-01 18:12:40 +0900

    add: common styled setting

---
+++ src/styles/variables.js
@@ -0,0 +1,8 @@
+export const bodyBgWhite = '#fafafa';
+export const fontColorBlack = '#262626';
+export const borderGrey = '#dbdbdb';
+export const nonSelectGrey = '#8e8e8e';
+export const linkBlue = '#00366b';
+export const buttonSkyBlue = '#0095f6';
+export const heartRed = '#ed4956';
+export const moreBlue = '#00376b';
