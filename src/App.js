6875872131e6580405423b3fe178abbdb5acc3d7
heejin lee <heejinlee07@gmail.com>
2020-09-02 14:01:50 +0900

    add: apply global-style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Test from './components/Test';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <div>
       <h1>I'mstagram</h1>
       <Test />
+      <GlobalStyles />
     </div>
   );
 }
