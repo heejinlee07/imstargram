1a8da0259e1e7c6f89031b1b09b7ab6ce0c07c6d
hodeng28 <ghepd28@gmail.com>
2020-09-02 21:21:24 +0900

    merge

---
+++ src/containers/ExploreContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import Explore from '../components/pages/Explore';
+
+const HomeContainer = () => {
+  return (
+    <div>
+      <Header />
+      <Explore />
+    </div>
+  );
+};
+
+export default HomeContainer;
