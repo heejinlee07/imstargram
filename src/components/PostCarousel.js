a6b052658109c423e8a0625b45395ff941614950
Mang-developer <chiwon618@gmail.com>
2020-09-03 17:02:50 +0900

    Feat: Made card carousel

--- src/components/PostCarousel.js
+++ src/components/PostCarousel.js
@@ -13,26 +13,15 @@ const images = [
   },
 ];
 
-// 스프레드 이미지 쓸건지 말건지? 개인적으로 반대.
-// 아이콘 url 따오거니
-// 폰트어썸 같은거 쓰거나
-
-export default function PostCarousel() {
+export default function PostCarousel({ imgUrlList }) {
   return (
     <>
       <ImageGallery
-        renderLeftNav={(onClick, disabled) => (
-          <button
-            disabled={disabled}
-            className="image-gallery-custom-left-nav"
-            onClick={onClick}
-          >
-            test
-          </button>
-        )}
         showPlayButton={false}
         showFullscreenButton={false}
-        items={images}
+        showBullets={true}
+        showThumbnails={false}
+        items={imgUrlList || images}
       />
     </>
   );
--- src/components/PostContents.js
+++ src/components/PostContents.js
@@ -3,26 +3,26 @@ import { HeartIcon, BookmarkIcon } from '../styles/commonIcons/SvgIcons';
 import PostCarousel from './PostCarousel';
 
 function PostContents() {
-  const [active, setActive] = useState({
-    heart: false,
-    bookmark: false,
-  });
+  // const [active, setActive] = useState({
+  //   heart: false,
+  //   bookmark: false,
+  // });
 
-  const heartClick = (e) => {
-    if (active.heart) setActive({ ...active, heart: false });
-    else setActive({ ...active, heart: true });
-  };
+  // const heartClick = (e) => {
+  //   if (active.heart) setActive({ ...active, heart: false });
+  //   else setActive({ ...active, heart: true });
+  // };
 
-  const bookmarkClick = (e) => {
-    if (active.bookmark) setActive({ ...active, bookmark: false });
-    else setActive({ ...active, bookmark: true });
-  };
+  // const bookmarkClick = (e) => {
+  //   if (active.bookmark) setActive({ ...active, bookmark: false });
+  //   else setActive({ ...active, bookmark: true });
+  // };
 
   return (
     <div>
       <PostCarousel />
-      <HeartIcon isActive={active.heart} />
-      <BookmarkIcon isActive={active.bookmark} />
+      {/* <HeartIcon isActive={active.heart} /> */}
+      {/* <BookmarkIcon isActive={active.bookmark} /> */}
     </div>
   );
 }
