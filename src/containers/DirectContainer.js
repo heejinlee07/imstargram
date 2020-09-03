14ccfcb2055d94de2793e67e1621839f5528da1c
hodeng28 <ghepd28@gmail.com>
2020-09-03 15:09:10 +0900

    fix: header code

--- src/containers/DirectContainer.js
+++ src/containers/DirectContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/header/Header';
 import Direct from '../components/pages/Direct';
 
-const DirectContainer = () => {
+const HomeContainer = () => {
   return (
     <div>
       <Header />
@@ -11,4 +11,4 @@ const DirectContainer = () => {
   );
 };
 
-export default DirectContainer;
+export default HomeContainer;
