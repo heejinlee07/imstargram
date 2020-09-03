414e14814ef35041d2e1eea43bc192fbf5f6f44e
hodeng28 <ghepd28@gmail.com>
2020-09-03 20:21:36 +0900

    feat: header profile nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Inputs from '../common/Inputs';
 import { Link } from 'react-router-dom';
-import { HeaderBlock, HeaderNav } from './Header.styles';
+import {
+  HeaderBlock,
+  HeaderNav,
+  HeaderWrap,
+  HeaderProfile
+} from './Header.styles';
+
+
 
 const Header = () => {
+
+  // const [headerProfileNavState, setHeaderProfileNavState] = useState()
+  // const openNavBar = 
   return (
-    <HeaderBlock>
-      <h1>logo</h1>
-      <Inputs placeholder="검색" />
-      <HeaderNav>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/direct">디엠</Link>
-        </li>
-        <li>
-          <Link to="/explore">랜덤 추천</Link>
-        </li>
-        <li>좋아요</li>
-        <li>프로필</li>
-      </HeaderNav>
-    </HeaderBlock>
+    <HeaderWrap>
+      <HeaderBlock>
+        <div>
+          <h1>logo</h1>
+          <div>
+            <Inputs placeholder="검색" />
+            <span></span>
+            <div></div>
+          </div>
+        </div>
+        <HeaderNav>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/direct">디엠</Link>
+          </li>
+          <li>
+            <Link to="/explore">랜덤 추천</Link>
+          </li>
+          <li>좋아요</li>
+          <li>프로필</li>
+          <HeaderProfile>
+            <li><span></span>프로필</li>
+            <li><span></span>저장됨</li>
+            <li><span></span>설정</li>
+            <div>로그아웃</div>
+          </HeaderProfile>
+        </HeaderNav>
+      </HeaderBlock>
+    </HeaderWrap>
   );
 };
 export default Header;
--- src/components/header/Header.styles.js
+++ src/components/header/Header.styles.js
@@ -1,21 +1,64 @@
 import styled from 'styled-components';
+import { borderGrey } from '../../styles/variables';
+
+export const HeaderWrap = styled.div`
+  width: 100%;
+  height: 54px;
+  background-color: lightgray;
+  border-bottom: solid 1.250px black;
+  position: relative;
+`;
 
 export const HeaderBlock = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   max-width: 975px;
-  padding: 0 20px;
   margin: 0 auto;
+  padding: 0 20px;
   background-color: red;
+  height: 100%;
+  h1 {
+    width: 360px;
+  }
+  div {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export const HeaderNav = styled.ul`
   display: flex;
   justify-content: space-between;
   background-color: lime;
-
   li {
     padding-left: 22px;
   }
 `;
+
+export const HeaderProfile = styled.ul`
+   width: 230px;
+   background: rgba(var(--d87,255,255,255),1);
+   position: absolute;
+   top: 54px;
+   border-radius: 6px;
+   text-align: left;
+   li, div {
+     display: block;
+     width: 170px;
+     width: 100%;
+     padding: 8px 16px;
+     font-size: 14px;
+     cursor: pointer;
+   }
+   li:hover {
+    background-color: ${(props) => props.InputColor || borderGrey};
+   }
+   div:hover {
+    background-color: ${(props) => props.InputColor || borderGrey};
+   }
+   div {
+     border-top: 1px solid #dbdbdb;
+   }
+`;
+
