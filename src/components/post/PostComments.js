76c513b24b07d062eb991de1b53e6fcf616edb7e
Mang-developer <chiwon618@gmail.com>
2020-09-04 15:28:08 +0900

    Feat: Made postComments list

--- src/components/post/PostComments.js
+++ src/components/post/PostComments.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Inputs from '../common/Inputs';
 
+import { PostCommentsBlock, PostCommentList } from './PostCommentsBlock.style';
+
+const commentsApi = ['댓', '글', '이', '야'];
+
 function PostComments() {
   return (
-    <div>
+    <PostCommentsBlock>
+      <PostCommentList>
+        {commentsApi.map((c) => (
+          <li>
+            <button>닉네임</button> {c}
+          </li>
+        ))}
+      </PostCommentList>
       <Inputs placeholder="댓글달기..." border={'none'} borderRadius={'0'} />
-    </div>
+    </PostCommentsBlock>
   );
 }
 
