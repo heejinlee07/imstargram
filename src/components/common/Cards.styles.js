92504eebec023a9bc9443d982b4011bea85926a8
YiJaeE <jinnn2181@gmail.com>
2020-09-07 18:47:03 +0900

    feat: common style unit change

--- src/components/common/Cards.styles.js
+++ src/components/common/Cards.styles.js
@@ -2,20 +2,14 @@ import styled from 'styled-components';
 import { bodyBgWhite, borderGrey } from '../../styles/variables';
 
 export const CardUi = styled.div`
-  width: ${(props) => props.width || 614}px;
-  height: ${(props) => props.height || 100}px;
+  width: ${(props) => props.width || 100}%;
+  height: ${(props) => props.height || 'auto'};
   background-color: ${(props) => props.bgColor || bodyBgWhite};
   color: ${(props) => props.color};
   text-decoration: none;
   text-align: left;
-  padding-top: ${(props) => props.paddingTop}px;
-  padding-bottom: ${(props) => props.paddingBottom}px;
-  padding-left: ${(props) => props.paddingLeft}px;
-  padding-right: ${(props) => props.paddingRight}px;
-  margin-top: ${(props) => props.marginTop}px;
-  margin-bottom: ${(props) => props.marginBottom}px;
-  margin-left: ${(props) => props.marginLeft}px;
-  margin-right: ${(props) => props.marginRigth}px;
+  padding: ${(props) => props.padding || '0px 0px 0px 0px'};
+  margin: ${(props) => props.margin || '0px 0px 0px 0px'};
   border-radius: ${(props) => props.borderRadius || 4}px;
   border: ${(props) => props.borderWeight || 1}px solid
     ${(props) => props.borderColor || borderGrey};
