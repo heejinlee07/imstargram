a64c3f29b8ef2c4dcd4791c389cea1abb39de1a7
Mang-developer <chiwon618@gmail.com>
2020-09-07 22:08:30 +0900

    Feat: Made Hover Event in Items

--- src/components/common/Items.style.js
+++ src/components/common/Items.style.js
@@ -1,18 +1,38 @@
 import styled from 'styled-components';
 
-import { bodyBgBlack, fontColorWhite } from '../../styles/variables';
+import {
+  bodyBgBlack,
+  fontColorWhite,
+  buttonSkyBlue,
+} from '../../styles/variables';
 
 export const ItemsUi = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  height: auto;
+  height: 200px;
   color: ${fontColorWhite};
-  background: ${bodyBgBlack + 80};
+  background: ${buttonSkyBlue};
+  cursor: pointer;
+
+  div {
+    opacity: 0;
+  }
+
+  &:hover {
+    background: linear-gradient(${bodyBgBlack + 80}, ${bodyBgBlack + 80})
+        no-repeat center/100%,
+      ${buttonSkyBlue};
+
+    div {
+      opacity: 100;
+    }
+  }
 `;
 
 export const LikeUi = styled.div`
   display: flex;
+  align-items: center;
 
   p {
     margin-left: 5px;
